21368899a6722250790d46cfd18120ae7eba4f8b
Nikita Kopylov <nik.kopylov.8080@mail.ru>
2023-06-29 00:51:47 +0300

    Add modal. Add portal. Change navbar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,7 +2,6 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { useTheme } from 'app/providers/ThemeProvider';
 import { AppRouter } from 'app/providers/router';
 import { Navbar } from 'widgets/Navbar';
-import './styles/index.scss';
 import { Sidebar } from 'widgets/Sidebar';
 import { Suspense } from 'react';
 
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,14 +1,37 @@
 import { classNames } from 'shared/lib/classNames/classNames';
+import { Modal } from 'shared/ui/Modal/Modal';
+import { useTranslation } from 'react-i18next';
+import { Button, ButtonTheme } from 'shared/ui/Button/Button';
+import { useCallback, useState } from 'react';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
   className?: string;
 }
 
-export const Navbar = ({ className }: NavbarProps) => (
-  <div className={classNames(cls.navbar, {}, [className])}>
-    <div className={cls.links}>
-      /
+export const Navbar = ({ className }: NavbarProps) => {
+  const { t } = useTranslation();
+  const [isAuthModal, setIsAuthModal] = useState(false);
+
+  const onToggleModal = useCallback(() => {
+    setIsAuthModal((prev) => !prev);
+  }, []);
+
+  return (
+    <div className={classNames(cls.navbar, {}, [className])}>
+      <Button
+        theme={ButtonTheme.CLEAR_INVERTED}
+        className={cls.links}
+        onClick={onToggleModal}
+      >
+        {t('Войти')}
+      </Button>
+      {/* eslint-disable-next-line i18next/no-literal-string */}
+      <Modal isOpen={isAuthModal} onClose={onToggleModal}>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        Nobis numquam repudiandae saepe sapiente tempora.
+        Corporis dolorum fugiat ipsum libero voluptatibus?
+      </Modal>
     </div>
-  </div>
-);
+  );
+};
