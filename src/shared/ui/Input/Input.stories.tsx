9ff7a8e51570ed7004ec23ce4041448a8f27c405
Nikita Kopylov <nik.kopylov.8080@mail.ru>
2023-07-02 00:06:33 +0300

    Add input. Add login form

---
+++ src/shared/ui/Input/Input.stories.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { Theme } from 'app/providers/ThemeProvider';
+import { Input } from './Input';
+
+export default {
+  title: 'shared/Input',
+  component: Input,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} as ComponentMeta<typeof Input>;
+
+const Template: ComponentStory<typeof Input> = (args) => <Input {...args} />;
+
+export const Normal = Template.bind({});
+Normal.args = {
+  placeholder: 'Введите текст',
+  value: 'Текст',
+};
+
+export const Dark = Template.bind({});
+Dark.args = {
+  placeholder: 'Введите текст',
+  value: 'Текст',
+};
+Dark.decorators = [ThemeDecorator(Theme.DARK)];
