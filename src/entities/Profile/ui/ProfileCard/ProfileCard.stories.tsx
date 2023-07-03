e756629c4cacea8dcf639572d3c887829c923ce9
Nikita Kopylov <nik.kopylov.8080@mail.ru>
2023-07-04 02:29:55 +0300

    Fix screenshots

--- src/entities/Profile/ui/ProfileCard/ProfileCard.stories.tsx
+++ src/entities/Profile/ui/ProfileCard/ProfileCard.stories.tsx
@@ -6,7 +6,7 @@ import avatar from 'shared/assets/tests/storybook.jpg';
 import { ProfileCard } from './ProfileCard';
 
 export default {
-  title: 'entities/LoginForm',
+  title: 'entities/ProfileCard',
   component: ProfileCard,
   argTypes: {
     backgroundColor: { control: 'color' },
