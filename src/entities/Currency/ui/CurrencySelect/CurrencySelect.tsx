306a426912ca2156be12e14ff52c411ed1173690
Nikita Kopylov <nik.kopylov.8080@mail.ru>
2023-07-04 00:48:35 +0300

    Upgrade profile. Create avatar. UI select. Add private routes

---
+++ src/entities/Currency/ui/CurrencySelect/CurrencySelect.tsx
@@ -0,0 +1,39 @@
+import { useTranslation } from 'react-i18next';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { Select } from 'shared/ui/Select/Select';
+import { memo, useCallback } from 'react';
+import { Currency } from '../../model/types/currency';
+
+interface CurrencySelectProps {
+  className?: string;
+  value?: Currency;
+  onChange?: (value: Currency) => void;
+  readOnly?: boolean;
+}
+
+const options = [
+  { value: Currency.RUB, content: Currency.RUB },
+  { value: Currency.USD, content: Currency.USD },
+  { value: Currency.EUR, content: Currency.EUR },
+];
+
+export const CurrencySelect = memo(({
+  className, value, onChange, readOnly,
+}: CurrencySelectProps) => {
+  const { t } = useTranslation();
+
+  const onChangeHandler = useCallback((value: string) => {
+    onChange?.(value as Currency);
+  }, [onChange]);
+
+  return (
+    <Select
+      className={classNames('', {}, [className])}
+      label={t('Укажите валюту')}
+      options={options}
+      value={value}
+      onChange={onChangeHandler}
+      readOnly={readOnly}
+    />
+  );
+});
--- src/shared/ui/Input/Input.tsx
+++ src/shared/ui/Input/Input.tsx
@@ -1,16 +1,17 @@
-import { classNames } from 'shared/lib/classNames/classNames';
+import { classNames, Mods } from 'shared/lib/classNames/classNames';
 import React, {
   InputHTMLAttributes, memo, useEffect, useRef, useState,
 } from 'react';
 import cls from './Input.module.scss';
 
-type HTMLInputProps = Omit<InputHTMLAttributes<HTMLInputElement>, 'value' | 'onChange'>
+type HTMLInputProps = Omit<InputHTMLAttributes<HTMLInputElement>, 'value' | 'onChange' | 'readOnly'>
 
 interface InputProps extends HTMLInputProps {
   className?: string;
-  value?: string;
+  value?: string | number;
   onChange?: (value: string) => void;
   autofocus?: boolean;
+  readOnly?: boolean;
 }
 
 export const Input = memo((props: InputProps) => {
@@ -21,12 +22,15 @@ export const Input = memo((props: InputProps) => {
     type = 'text',
     placeholder,
     autofocus,
+    readOnly,
     ...otherProps
   } = props;
 
   const ref = useRef<HTMLInputElement>(null);
   const [isFocused, setIsFocused] = useState(false);
   const [caretPosition, setCaretPosition] = useState(0);
+
+  const isCaretVisible = isFocused && !readOnly;
 
   useEffect(() => {
     if (autofocus) {
@@ -52,8 +56,12 @@ export const Input = memo((props: InputProps) => {
     setCaretPosition(e?.target?.selectionStart || 0);
   };
 
+  const mods: Mods = {
+    [cls.readOnly]: readOnly,
+  };
+
   return (
-    <div className={classNames(cls.inputWrapper, {}, [className])}>
+    <div className={classNames(cls.inputWrapper, mods, [className])}>
       {placeholder && (
         <div className={cls.placeholder}>
           {`${placeholder}>`}
@@ -69,9 +77,10 @@ export const Input = memo((props: InputProps) => {
           onBlur={onBlur}
           onFocus={onFocus}
           onSelect={onSelect}
+          readOnly={readOnly}
           {...otherProps}
         />
-        {isFocused && (
+        {isCaretVisible && (
           <span
             className={cls.caret}
             style={{ left: `${caretPosition * 9}px` }}
