04b6c2e833f1c994a8260352dc40b172eaae1dbe
Nikita Kopylov <nik.kopylov.8080@mail.ru>
2023-07-05 01:47:09 +0300

    Update private routes

--- src/shared/config/routeConfig/routeConfig.tsx
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -4,7 +4,7 @@ import { AboutPage } from 'pages/AboutPage';
 import { NotFoundPage } from 'pages/NotFoundPage';
 import { ProfilePage } from 'pages/ProfilePage';
 
-type AppRoutesProps = RouteProps & { authOnly?: boolean };
+export type AppRoutesProps = RouteProps & { authOnly?: boolean };
 
 export enum AppRoutes {
   MAIN = 'main',
