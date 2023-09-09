18730d35b27362ca76a0c21d445f0a6bcccb017d
Nikita Kopylov <nik.kopylov.8080@mail.ru>
2023-09-09 20:49:24 +0300

    Prepare project for deploy

--- src/features/AuthByUsername/ui/LoginForm/LoginForm.async.ts
+++ src/features/AuthByUsername/ui/LoginForm/LoginForm.async.ts
@@ -1,8 +1,4 @@
 import { FC, lazy } from 'react';
 import { LoginFormProps } from './LoginForm';
 
-export const LoginFormAsync = lazy<FC<LoginFormProps>>(() => new Promise((resolve) => {
-  // @ts-ignore
-  // For test, don't scare)
-  setTimeout(() => resolve(import('./LoginForm')), 1500);
-}));
+export const LoginFormAsync = lazy<FC<LoginFormProps>>(() => import('./LoginForm'));
