12bfeed2b27c812061220ab4883cb9d125de0760
Nikita Kopylov <nik.kopylov.8080@mail.ru>
2023-10-15 00:22:33 +0300

    Fix storybook. Update UI tests results

--- src/features/ArticleRating/ui/ArticleRating/ArticleRating.stories.tsx
+++ src/features/ArticleRating/ui/ArticleRating/ArticleRating.stories.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 
 import ArticleRating from './ArticleRating';
+import { StoreDecorator } from '@/shared/config/storybook/StoreDecorator/StoreDecorator';
 
 export default {
-  title: 'shared/ArticleRating',
+  title: 'features/ArticleRating',
   component: ArticleRating,
   argTypes: {
     backgroundColor: { control: 'color' },
@@ -16,4 +17,49 @@ const Template: ComponentStory<typeof ArticleRating> = (args) => (
 );
 
 export const Normal = Template.bind({});
-Normal.args = {};
+Normal.args = {
+  articleId: '1',
+};
+Normal.decorators = [
+  StoreDecorator({
+    user: {
+      authData: {
+        id: '1',
+      },
+    },
+  }),
+];
+Normal.parameters = {
+  mockData: [
+    {
+      url: `${__API__}/article-ratings?userId=1&articleId=1`,
+      method: 'GET',
+      status: 200,
+      response: [{ rate: 4 }],
+    },
+  ],
+};
+
+export const WithoutRate = Template.bind({});
+WithoutRate.args = {
+  articleId: '1',
+};
+WithoutRate.decorators = [
+  StoreDecorator({
+    user: {
+      authData: {
+        id: '1',
+      },
+    },
+  }),
+];
+WithoutRate.parameters = {
+  mockData: [
+    {
+      url: `${__API__}/article-ratings?userId=1&articleId=1`,
+      method: 'GET',
+      status: 200,
+      response: [],
+    },
+  ],
+};
