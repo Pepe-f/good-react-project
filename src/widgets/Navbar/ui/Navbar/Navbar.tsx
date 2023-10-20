b595c1a8ca9b088414ba501f26e14aa3261f353d
Nikita Kopylov <nik.kopylov.8080@mail.ru>
2023-10-20 22:13:02 +0300

    Refactor router

--- src/widgets/Navbar/ui/Navbar/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar/Navbar.tsx
@@ -7,13 +7,13 @@ import { LoginModal } from '@/features/AuthByUsername';
 import { getUserAuthData } from '@/entities/User';
 import { Text, TextTheme } from '@/shared/ui/Text/Text';
 import { AppLink, AppLinkTheme } from '@/shared/ui/AppLink/AppLink';
-import { RoutePath } from '@/shared/config/routeConfig/routeConfig';
 import { HStack } from '@/shared/ui/Stack';
 import { NotificationButton } from '@/features/NotificationButton';
 import { AvatarDropdown } from '@/features/AvatarDropdown';
 import { Drawer } from '@/shared/ui/Drawer/Drawer';
 import { NotificationList } from '@/entities/Notification';
 import cls from './Navbar.module.scss';
+import { RoutePath } from '@/shared/consts/router';
 
 interface NavbarProps {
   className?: string;
