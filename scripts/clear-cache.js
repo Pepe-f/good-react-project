274ff83f6f299da247a21b50ca782549b45bf83c
Nikita Kopylov <nik.kopylov.8080@mail.ru>
2023-10-24 23:22:38 +0300

    Upgrade webpack config. Add babel cache

---
+++ scripts/clear-cache.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const path = require('path');
+
+fs.rmSync(path.resolve(__dirname, '..', 'node_modules', '.cache'), {
+  recursive: true,
+  force: true,
+});
+
+console.log('Clear cache done');
